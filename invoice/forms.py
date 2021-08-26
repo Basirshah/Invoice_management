103481fce91de766dfc863b6d0d308be3c40b0ae
Basir Ahmed Shah <30934839+Basirshah@users.noreply.github.com>
2021-08-26 22:58:54 +0500

    Add files via upload

---
+++ invoice/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Invoice
+
+class InvoiceForm(forms.ModelForm):
+	class Meta:
+		model = Invoice
+		fields = ['name', 'phone_number', 'invoice_date',
+				'line_one', 'line_one_quantity', 'line_one_unit_price', 'line_one_total_price',
+				'total', 'paid', 'invoice_type'
+				 ]
+class InvoiceSearchForm(forms.ModelForm):
+	class Meta:
+		model = Invoice
+		fields = ['invoice_number', 'name']				 
